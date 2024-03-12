de36597799250ddb11e405b4c9ff9ec984264441
NwType <kevin19992309@gmail.com>
2024-03-12 10:24:48 -0400

    Clases para SistemaVentas.Modelos

---
+++ SistemaVentas/SistemaVentas.Modelos/Ingresocs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVentas.Modelos
+{
+    public class Ingresocs
+    {
+        public int IdIngreso { get; set; }
+        public int Proveedor { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public int Total {  get; set; }
+        public string Estado { get; set; }
+    }
+}
