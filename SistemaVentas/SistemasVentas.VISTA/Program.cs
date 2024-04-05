1016d0dc5593e91172a3b6dca27e5e7f4a8e4205
NwType <kevin19992309@gmail.com>
2024-04-05 00:01:19 -0400

    mensaje

--- SistemaVentas/SistemasVentas.VISTA/Program.cs
+++ SistemaVentas/SistemasVentas.VISTA/Program.cs
@@ -11,37 +11,10 @@ namespace SistemasVentas.VISTA
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            //Application.Run(new PersonaVistas.PersonaListarVista());
-            //Application.Run(new PersonaVistas.InsertarPersonaVista());
-            //Application.Run(new ClientesVistas.ClientesListarVista());
-            //Application.Run(new ClientesVistas.InsertarClienteVista());
-            //Application.Run(new UsuarioVistas.UsuarioListarVista());
-             //Application.Run(new UsuarioVistas.InsertarUsuarioVista());
-           //Application.Run(new UsuarioVistas.UsuarioInsertarVistasP());
-            //Application.Run(new ProveedorVistas.ProveedorListarVista());
-            //Application.Run(new ProveedorVistas.InsertarProveedorVista());
-            //Application.Run(new ProductoVistas.ProductoListarVista());
-            //Application.Run(new ProductoVistas.InsertarProductoVista());
-            //Application.Run(new MarcaVistas.MarcaListarVista());
-            //Application.Run(new MarcaVistas.InsertarMarcaVista());
-            //Application.Run(new ProveeVistas.ProveeListarVistas());
-            //Application.Run(new ProveeVistas.InsertarProveeVista());
-            //Application.Run(new DetalleingVistas.DetalleingListarVista());
-            //Application.Run(new DetalleingVistas.InsertarDetalleingVista());
-            //Application.Run(new IngresoVistas.IngresoListarVista());
-            //Application.Run(new IngresoVistas.InsertarIngresoVista());
-            //Application.Run(new TipoProdVistas.TipoProdListarVista());
-            //Application.Run(new TipoProdVistas.InsertarTipoProdVista());
-            //Application.Run(new UsuarioRolVista.UsuarioRolListarVista());
-            //Application.Run(new UsuarioRolVista.InsertarUsuarioRolVista());
-            //Application.Run(new VentaVistas.VentaListarVista());
-            //Application.Run(new VentaVistas.InsertarVentaVista());
-            //Application.Run(new DetalleVentaVistas.DetalleVentaListarVenta());
-            //Application.Run(new DetalleVentaVistas.InsertarDetalleVentaVista());
-            //Application.Run(new RolVistas.RolListarVista());
-            //Application.Run(new RolVistas.RolAgregarVista());
-            Application.Run(new AdministradorAlmacenVista.AdministradorAlmacenVista());
-            //Application.Run(new AdministradorVista.AdministradorVista());
+            Application.Run(new VentaVistas.VentaListarVista());
+            Application.Run(new VentaVistas.InsertarVentaVista());
+            Application.Run(new DetalleVentaVistas.DetalleVentaListarVenta());
+            Application.Run(new DetalleVentaVistas.InsertarDetalleVentaVista());
 
         }
     }
